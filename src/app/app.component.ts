05fa4181ca2afefff198e1f4f1741e4f3e7f145c
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-11-30 16:20:05 +0500

    chore: refactor indent style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MTS_hackathon';
+    public title: string = 'MTS_hackathon';
 }
